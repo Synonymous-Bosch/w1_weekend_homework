1254522e56b81eb1209c8ed7270b95d05fd54ab6
Steven <stevenjohnmorgan@gmail.com>
2023-05-19 11:26:12 +0100

    second commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -9,3 +9,18 @@ def get_total_cash(self):
 def add_or_remove_cash(self, amount):
     self["admin"]["total_cash"] += amount
 
+def get_pets_sold(self):
+    return self["admin"]["pets_sold"]
+
+def increase_pets_sold(self, pets_sold):
+    self["admin"]["pets_sold"] += pets_sold
+
+def get_stock_count(self):
+    return len(self["pets"])
+
+def get_pets_by_breed(self, breed):
+    pets = []
+    for pet in self["pets"]:
+        if breed == pet["breed"]:
+            pets.append(pet)
+    return pets
